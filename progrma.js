3eb40be6aab3cc9d30e65238a7a017031e7b25e9
noorulain88 <noorulainnoorulain86@gmail.com>
2025-11-04 20:36:27 +0500

    assignment 11

---
+++ progrma.js
@@ -0,0 +1,167 @@
+console.log("Hello World!");
+
+// program 01
+
+// var Name=prompt("Ente your name");
+// console.log("user name is ",Name);
+
+//  var age=prompt("Enter your age");
+// console.log("user age is " ,age);
+//  program 02
+
+for(var i=0;i<=99;i++){
+    console.log("the number from 0 to 100 in for loop is ");
+}
+
+// program 03
+var i=0;
+while(i<=49){
+    console.log("the number from 0 to 50 in while loop is ")
+    i++
+}
+
+
+
+
+
+// program 04
+
+// var num1=parseFloat(prompt("enter a number number one between 1 to 50"));
+// var num2=parseFloat(prompt("enter a number number two between 1 to 50"));
+// var operater=prompt("enter a operater b/w +,-,*,/,%,")
+// switch( operater){
+//         case "+":
+//     result =num1 + num2;
+//         console.log("The addition of both number is ",result);
+
+//     break;
+//     case "-":
+//            result =num1 - num2;
+//         console.log("The subtraction of both number is ",result);
+//         break;
+//             case "*":
+//            result =num1 * num2;
+//         console.log("The multiplication of both number is ",result);
+//         break;
+//                case "/":
+//            result =num1 / num2;
+//         console.log("The   division  of both number is ",result);
+//         break;
+//                 case "%":
+//            result =num1 % num2;
+//         console.log("The  modulus  of both   number is ",result);
+//         break;
+
+
+// }
+
+// program 05
+var num1=parseFloat(prompt("enter a number number one between 1 to 50"));
+var num2=parseFloat(prompt("enter a number number two between 1 to 50"));
+
+for( var num1=0; num1>=50; num2>=50  )
+  {
+    console.log("the addition of two number is ", num1 + num2);
+}
+
+// program 06
+
+var ans = "0";
+do{
+    var ans="0";
+    ans=prompt("do you u want to continue? if yes then press 0");
+} while(ans=="0")  
+    if(ans!="0")
+console.log("user dont want to continue ");
+
+
+// program 07
+
+var i=10;
+while(i>1){
+    console.log("the number from 10 to 1 in while loop is ", );
+    i--
+}
+
+
+// program 08
+
+var password=prompt("enter your password");
+password="123";
+while( password!="123"){
+    console.log(" correct password");
+    
+}
+    console.log("wrong password please enter correct password");
+
+    // program 09
+
+    var days=prompt("enter a day number between 1 to 7");
+    switch( days){
+        case "monday":
+  
+    console.log("today is monday");
+
+    break;
+    case "tuesday":
+        console.log("Today is tuesday ",);
+        break;
+            case "wednesday":
+        console.log("Today is wednesday ",);
+        break;
+                  case "thursday":
+        console.log("Today is thursday",);
+        break;
+                  case "friday":
+        console.log("Today is friday ",);
+        break;
+                  case "saturday":
+        console.log("Today is saturday ",);
+        break;
+                  case "sunday":
+        console.log("Today is sunday ",);
+        break;
+    
+
+
+}
+// program 10
+
+ var marks=prompt("enter ur marks");
+    switch( marks){
+        case "90-100":
+  
+    console.log("your grade is A+");
+
+    break;
+    case "tuesday":
+        console.log("Today is tuesday ",);
+        break;
+            case "wednesday":
+        console.log("Today is wednesday ",);
+        break;
+                  case "thursday":
+        console.log("Today is thursday",);
+        break;
+                  case "friday":
+        console.log("Today is friday ",);
+        break;
+                  case "saturday":
+        console.log("Today is saturday ",);
+        break;
+                  case "sunday":
+        console.log("Today is sunday ",);
+        break;
+    
+
+
+}
+
+
+
+
+
+
+
+
+
